ad46b4248a9257105cab9f7643a3748923909faf
bysxx <0415white@naver.com>
2022-01-30 22:19:38 +0900

    BaekJoon - BinarySearch 1920 풀이

---
+++ Baek/BaekJoon/BinarySearch/1920.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tmp;
+
+    int n;
+    cin >> n;
+    vector<int> targetVec;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        targetVec.push_back(tmp);
+    }
+
+    sort(targetVec.begin(), targetVec.end()); // Target Sort
+
+    int m;
+    cin >> m;
+    vector<int> inputVec;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> tmp;
+        inputVec.push_back(tmp);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        if (binary_search(targetVec.begin(), targetVec.end(), inputVec[i]) == true) // BInary Search
+        {
+            printf("%o\n", 1);
+        }
+        else
+        {
+            printf("%o\n", 0);
+        }
+    }
+}
